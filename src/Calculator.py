3aa9841a4457c98ba4b70b2779abcb30fb37e8c6
Emad <eaa38@njit.edu>
2021-03-22 17:07:22 -0400

    Fix: fixed Calculator.py, added test methods

--- src/Calculator.py
+++ src/Calculator.py
@@ -22,12 +22,12 @@ class Calculator:
     def add(self, a, b):
         return addition(a,b)
     def subtract(self, a, b):
-        return addition(a,b)
+        return subtraction(a,b)
     def multiply(self, a, b):
-        return addition(a,b)
+        return multiplication(a,b)
     def divide(self, a, b):
-        return addition(a,b)
-    def sqr(self, a, b):
-        return addition(a,b)
-    def sqrRoot(self, a, b):
-        return addition(a,b)
+        return division(a,b)
+    def sqr(self, a):
+        return square(a)
+    def sqrRoot(self, a):
+        return squareRoot(a)
--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -2,18 +2,32 @@ import unittest
 from Calculator import Calculator
 
 class MyTestCase(unittest.TestCase):
-    def test_instantiate_claculator(self):
-        calculator = Calculator()
-        self.assertIsInstance(calculator, Calculator)
 
-    def test_result_property_calculator(self):
-        calculator = Calculator()
-        self.assertEqual(calculator.result, 4)
+    def setUp(self) -> None:
+        self.calculator = Calculator()
+
+    def test_instantiate_claculator(self):
+        self.assertIsInstance(self.calculator, Calculator)
 
     def test_add_method_calculatorr(self):
-        calculator = Calculator()
-        self.assertEqual(calculator.add(2,2), 4)
-        self.assertEqual(calculator.result, 4)
+        self.assertEqual(self.calculator.add(2,2), 4)
+
+    def test_subtract_method_calculator(self):
+        self.assertEqual(self.calculator.subtract(2,2), 0)
+
+    def test_multiply_method_calculator(self):
+        self.assertEqual(self.calculator.multiply(2,2), 4)
+
+    def test_divide_method_calculator(self):
+        self.assertEqual(self.calculator.divide(4,2), 2)
+
+    def test_sqr_method_calculator(self):
+        self.assertEqual(self.calculator.sqr(2), 4)
+
+    def test_sqrRoot_method_calculator(self):
+        self.assertEqual(self.calculator.sqrRoot(4), 2)
+
+
 
 if __name__ == '__main__':
     unittest.main()
