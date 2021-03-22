d0d5c4dd06feaee1ee6d833df2d61665bfe2e4bc
Emad <eaa38@njit.edu>
2021-03-22 16:27:09 -0400

    Feature: all methodes

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,13 +1,33 @@
+import math
 
 def addition(a, b):
     return a + b
+def subtraction(a, b):
+    return a - b
+def multiplication(a, b):
+    return a * b
+def division(a, b):
+    return a / b
+def square(a):
+    return a * a
+def squareRoot(a):
+    return math.sqrt(a)
 
 class Calculator:
     result = 0
 
     def __init__(self):
-        x = 2 +2
-        self.result = x
         pass
+
     def add(self, a, b):
+        return addition(a,b)
+    def subtract(self, a, b):
+        return addition(a,b)
+    def multiply(self, a, b):
+        return addition(a,b)
+    def divide(self, a, b):
+        return addition(a,b)
+    def sqr(self, a, b):
+        return addition(a,b)
+    def sqrRoot(self, a, b):
         return addition(a,b)
